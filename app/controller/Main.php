626dfa154b941e7d7bfecfd42b55c5f55df58f1d
DeepAQ <i@aq741.com>
2017-12-15 22:44:41 +0800

    card search

--- app/controller/Main.php
+++ app/controller/Main.php
@@ -2,12 +2,22 @@
 
 namespace app\controller;
 
+use app\model\Card;
 use BestLang\core\controller\BLController;
+use BestLang\core\util\BLRequest;
 
 class Main extends BLController
 {
     public function index()
     {
-        return $this->view('index');
+        $kw = BLRequest::get('kw');
+        if (isset($kw)) {
+            $cards = Card::query()->where('tags', 'LIKE', '%' . $kw . '%')->limit(12)->get();
+            $title = '搜索结果 - ' . $kw;
+        } else {
+            $cards = Card::query()->orderBy('random()')->limit(12)->get();
+            $title = '随机卡片';
+        }
+        return $this->view('index', ['cards' => $cards, 'title' => $title]);
     }
 }
